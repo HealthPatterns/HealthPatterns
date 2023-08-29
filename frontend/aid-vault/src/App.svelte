fa3ef034603f73bcbbd44ebf7a5f40790385732b
wmneco <>
2023-08-29 17:18:53 +0200

    Delete more example stuff

--- frontend/aid-vault/src/App.svelte
+++ frontend/aid-vault/src/App.svelte
@@ -4,27 +4,7 @@
 
 <main>
   <h1 class="text-3xl font-bold underline text-amber-600">AID-Vault</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
 </style>
