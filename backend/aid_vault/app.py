877b25e22af06fe6ce64bc3fc42bfe3751258664
mansf-osk <mansfeld.oskar@gmail.com>
2023-09-22 23:43:03 +0200

    added code to actually create the db tables

--- backend/aid_vault/app.py
+++ backend/aid_vault/app.py
@@ -23,9 +23,9 @@ def create_app():
 
 def configure_extensions(app):
     db.init_app(app)
-    # with app.app_context():
-    #     db.create_all()
-    migrate.init_app(app, db)
+    with app.app_context():
+        db.create_all()
+    # migrate.init_app(app, db)
     ma.init_app(app)
 
 def register_blueprints(app):
