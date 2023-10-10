a96996e3052d5734cd28b8418b3eba2cf8c5a01d
ehrenle <leon.ehrensberger@gmail.com>
2023-10-10 12:21:47 +0200

    First draft API call

--- frontend/aid-vault/src/App.svelte
+++ frontend/aid-vault/src/App.svelte
@@ -21,6 +21,46 @@
     console.log(isTracking);
   }
 
+  //API
+  async function apiStartTracking(apiToken) {
+  const url = "http://localhost/";
+
+  const requestData = {
+    "currentTime": new Date().toISOString()
+  };
+
+  const options = {
+    method: 'POST',
+    headers: {
+      'Authorization': 'Bearer ' + apiToken,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(requestData)
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const statusCode = response.status;
+    if (statusCode === 200) {
+      console.log("API call 'tracking started' successful.");
+    } else {
+      console.log("Error during API call 'tracking started'.");
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+
+  function apiStopTracking () {
+    
+  }
+
+  function apiDetails () {
+    
+  }
+
+
 </script>
 
 <main style="background-color: { enableAddDetails ? "#F2F1E8" : "#fff" }">
