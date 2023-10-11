8e129489599e6122406a12ace0e7cee40b481e7e
mansf-osk <mansfeld.oskar@gmail.com>
2023-10-11 22:33:05 +0200

    enabled CORS for http://localhost

--- backend/aid_vault/main.py
+++ backend/aid_vault/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .db.base_class import Base
 from .db.database import engine
@@ -9,6 +10,15 @@ Base.metadata.create_all(bind=engine)
 
 # init app
 app = FastAPI()
+
+# setup CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http:/localhost"],
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
 
 # include api
 app.include_router(api)
