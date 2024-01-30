c04095e51bfbce3620431cd0a80e94b1cb010eac
ehrenle <leon.ehrensberger@gmail.com>
2024-01-30 17:34:04 +0100

    login screen design improved

--- frontend/aid-vault/src/lib/LoginScreen.svelte
+++ frontend/aid-vault/src/lib/LoginScreen.svelte
@@ -49,18 +49,16 @@
 {#if enabled}
 <div id=LoginScreen>
     <h1>Login</h1>
-    <form on:submit|preventDefault={handleLogin}>
-        <label for="username">Nutzername:</label>
-        <input type="text" id="username" bind:value={username} />
-
-        <label for="password">Passwort:</label>
-        <input type="password" id="password" bind:value={password} />
-
-        <button type="submit">Login</button>
-    </form>
-    {#if errorMessage}
-    <p style="color: red;">{errorMessage}</p>
-    {/if}
+    <div id=LoginFields>
+        <form on:submit|preventDefault={handleLogin}>
+            <input type="text" id="username" bind:value={username} placeholder="Nutzername"/>
+            <input type="password" id="password" bind:value={password} placeholder="Passwort"/>
+            <button type="submit">Login</button>
+        </form>
+        {#if errorMessage}
+        <p style="color: red;">{errorMessage}</p>
+        {/if}
+    </div>
 </div>
 {/if}
 
@@ -70,19 +68,53 @@
     height: 90%;
     width: 80%;
     flex-direction: column;
+    justify-content: center;
+}
+#LoginFields {
+    height: 70%;
 }
 h1 {
-    font-size: x-large;
+    font-size: xx-large;
     font-weight: 500;
-}
-input {
-    border: 1px solid;
-    border-radius: 4px;
-    border-color: #0D698B;
+    align-items: center;
+    justify-content: center;
+    display: flex;
+    height: 30%;
 }
 button {
-    background-color: #0D698B;
-    color: #fff;
-    border-radius: 4px;
+    background-color: #0d698b;
+    color: #f2f1e8;
+    border: 0;
+    border-radius: 0.4rem;
+    font-size: x-large;
+    font-weight: 500;
+    padding: 0.5rem;
+    margin-top: 2rem;
+    width: 100%;
+    align-items: center;
+    justify-content: center;
+    display: flex;
+}
+input {
+    background-color: #E9EFF3;
+    border: 0;
+    border-radius: 0.4rem;
+    font-weight: 500;
+    padding: 0.75rem;
+    margin-top: 1.5rem;
+    width: 100%;
+    align-items: center;
+    justify-content: center;
+    display: flex;
+}
+input::placeholder {
+    color: #8e9294;
+}
+input:focus {
+    outline-color: #0D698B;
+}
+p {
+    margin-top: 1rem;
+    text-align: center;
 }
 </style>
