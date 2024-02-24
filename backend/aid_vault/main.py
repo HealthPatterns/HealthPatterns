6282c7337c74ee8642a11a052e87774d87b8f164
mansf-osk <mansfeld.oskar@gmail.com>
2024-02-24 18:56:52 +0100

    chore: reworked handling of api path
    added base_path variable to have it stored in one place.
    base_path is used as a prefix for our main APIRouter that combines the
    others and for the path to the docs.

--- backend/aid_vault/main.py
+++ backend/aid_vault/main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from .db.base_class import Base
 from .db.database import engine, SessionLocal
-from .api.api import api
+from .api.api import api, base_path
 from .pre_start import create_test_user
 
 # create db tables
@@ -14,18 +14,20 @@ db = SessionLocal()
 print(create_test_user(db))
 
 # init app
-app = FastAPI(title="Health Patterns",
-              description="API for the project Health Patterns",
-              docs_url='/api/docs',
-              openapi_url='/api/openapi.json')
+app = FastAPI(
+    title="Health Patterns",
+    description="API for the project Health Patterns",
+    docs_url=base_path + "/docs",
+    openapi_url=base_path + "/openapi.json",
+)
 
 # setup CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost","https://localhost"],
-    allow_credentials = True,
-    allow_methods = ["*"],
-    allow_headers = ["*"]
+    allow_origins=["http://localhost", "https://localhost"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # include main api
